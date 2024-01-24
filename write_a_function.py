569aa53487af63fab44b940c5f176f6903bec4fc
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-25 00:59:45 +0800

    pseudocode

--- write_a_function.py
+++ write_a_function.py
@@ -13,3 +13,16 @@
 # exponent = 4
 # 5 raises to the power of 4 is: 625 
 # i.e. (5 *5 * 5 *5 = 625)
+
+# --------------------------------------------------------------------
+
+# Pseudocode 
+
+# Create an function named exponent (base,exp)
+# Inside the function, create a variable named result and set it to 1 
+# Inside the function, create a loop that will multiply the result to the base
+# Return the result 
+# Create a variable to let the user input the base number
+# Create a variable to let the user input the exponent number 
+# Create a variable to place the inputted values in the function
+# Print the result 
